8159dcac730f36dcad4ecfec98680ff0883caca0
Aybek Kembayev <aheadofthepack15@gmail.com>
2017-10-22 20:39:56 +0600

    initial commit

---
+++ src/Solver.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, TextInput } from 'react-native';
+import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
+
+class Solver extends Component {
+  state = { targetNum: '', numbers: [] };
+
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+}
+
+export default Solver;
